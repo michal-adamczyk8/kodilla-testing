b88c317c7520cce6c5375d3dc2f288f2302db56f
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-01-11 18:54:58 +0100

    First tests

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie po testowania oprogramowania");
-    }
+        public static void main (String[] args) {
+
+                SimpleUser simpleUser = new SimpleUser("theForumUser");
+                String result = simpleUser.getUsername();
+
+                if (result.equals("theForumUser")) {
+                        System.out.println("test OK");
+                }
+                else {
+                        System.out.println("Coś poszło nie tak!");
+                }
+
+                Calculator calculator = new Calculator(5,674);
+                int result1 = calculator.add();
+
+                if (result1 == 674+5){
+                        System.out.println("Drugi test poszedł bez zarzutów!");
+                }
+                else {
+                        System.out.println("Błąd");
+                }
+        }
 }
